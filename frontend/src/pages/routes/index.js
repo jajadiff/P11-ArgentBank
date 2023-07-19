832a38313c20a8779bdb17877452f4b776403543
Jordan <jordanfurneri57@gmail.com>
2023-07-20 01:07:19 +0200

    add PrivateRoute user page

--- frontend/src/pages/routes/index.js
+++ frontend/src/pages/routes/index.js
@@ -1,14 +1,25 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import IndexPage from "../Index";
 import Sign from "../Sign";
 import User from "../User";
+
+// Check si token = true
+const isLogIn = () => {
+  const token = localStorage.getItem("token");
+  return !!token;
+};
+
+// Redirection
+const PrivateRoute = ({ element }) => {
+  return isLogIn() ? element : <Navigate to="/sign" />;
+};
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<IndexPage />} />
       <Route path="/sign" element={<Sign />} />
-      <Route path="/user" element={<User />} />
+      <Route path="/user" element={<PrivateRoute element={<User />} />} />
     </Routes>
   );
 };
