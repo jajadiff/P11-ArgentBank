4a5505220ccfee4267a4dc0b98f47dcfb3ed8e69
Jordan <jordanfurneri57@gmail.com>
2023-07-20 00:42:07 +0200

    Update store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./reducers";
+import rootReducer from "./reducers";
 
 const store = configureStore({
-  reducer: {
-    auth: authReducer,
-  },
+  reducer: rootReducer,
   devTools: true,
 });
 
