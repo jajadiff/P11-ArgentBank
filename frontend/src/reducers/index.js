7caad1f00982f9c42b35d0dbb2fd95505ab88e15
Jordan <jordanfurneri57@gmail.com>
2023-07-20 10:22:21 +0200

    add Reducers

--- frontend/src/reducers/index.js
+++ frontend/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import logUser from "./logUser";
+import dataUser from "./dataUser";
 
 const rootReducer = combineReducers({
   log: logUser,
+  user: dataUser,
 });
 
 export default rootReducer;
